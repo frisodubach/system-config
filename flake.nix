c9e275c00de9a2032b54b2a9a27a65ab16db5ac5
System administrator <ffcdubach@gmail.com>
2023-07-30 14:39:41 +0200

    changed nix settings section

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Automatic system configuration using flakes for electron";
 
-  nix.settings = {
+  nixConfig = {
     substituters = [ "https://hyprland.cachix.org" ];
     trusted-public-keys =
       [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
