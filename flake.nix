c0c1156bde277a1b54065f3e895a5c5c552d0b35
System administrator <ffcdubach@gmail.com>
2023-07-30 14:41:26 +0200

    forgot to remove from flake.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,6 @@
 {
   description = "Automatic system configuration using flakes for electron";
 
-  nixConfig = {
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys =
-      [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-  };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
