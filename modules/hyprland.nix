0c8fff143f45d714f2bf437a14116a400dd5d73d
System administrator <ffcdubach@gmail.com>
2023-07-30 22:36:14 +0200

    removed wlr portal

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -34,7 +34,7 @@
 
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
+    # wlr.enable = true;
     # extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
     gtkUsePortal = true;
   };
