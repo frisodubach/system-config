14994b2ffee376bc332b3feef8eb896c27a60cba
System administrator <ffcdubach@gmail.com>
2023-08-20 13:51:38 +0200

    restructure & eww update

--- modules/eww/eww.nix
+++ modules/eww/eww.nix
@@ -9,46 +9,22 @@
     # programs.eww.enable = true;
 
     # Config
-    home.file.".config/eww/eww.scss" = {
-      source = ./eww.scss;
-      enable = true;
-    };
+    # home.file.".config/eww/eww.scss" = {
+    #   source = ./eww.scss;
+    #   enable = true;
+    # };
 
-    home.file.".config/eww/eww.yuck" = {
-      source = ./eww.yuck;
-      enable = true;
-    };
+    # home.file.".config/eww/eww.yuck" = {
+    #   source = ./eww.yuck;
+    #   enable = true;
+    # };
 
     #scripts
-    home.file.".config/eww/scripts" = {
-      source = ./scripts;
+    home.file.".config/eww" = {
+      source = ./bar1;
       executable = true;
       recursive = true;
     };
 
-    # home.file.".config/eww/scripts/battery.sh" = {
-    #   source = ./scripts/battery.sh;
-    #   executable = true;
-    # };
-
-    # home.file.".config/eww/scripts/wifi.sh" = {
-    #   source = ./scripts/wifi.sh;
-    #   executable = true;
-    # };
-
-    # home.file.".config/eww/scripts/brightness.sh" = {
-    #   source = ./scripts/brightness.sh;
-    #   executable = true;
-    # };
-
-    # home.file.".config/eww/scripts/workspaces.sh" = {
-    #   source = ./scripts/workspaces.sh;
-    #   executable = true;
-    # };
-
-    # home.file.".config/eww/scripts/workspaces.lua" = {
-    #   source = ./scripts/workspaces.lua;
-    #   executable = true;
-    # };
   };
 }
