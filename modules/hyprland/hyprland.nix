ba56c614acdc39ad266d070fc08444da6f32d3e1
System administrator <ffcdubach@gmail.com>
2023-09-01 19:06:07 +0200

    rm swaylock import

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -3,7 +3,7 @@
 {
 
   # === Hyprland ===
-  imports = [ inputs.hyprland.nixosModules.default ./swaylock.nix ];
+  imports = [ inputs.hyprland.nixosModules.default ];
 
   nix.settings = {
     substituters = [ "https://hyprland.cachix.org" ];
@@ -35,12 +35,7 @@
     # DISPLAY = ":0";
   };
 
-  xdg.portal = {
-    enable = true;
-    # wlr.enable = true;
-    # extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-    # gtkUsePortal = true;
-  };
+  xdg.portal = { enable = true; };
 
   hardware.opengl = {
     enable = true;
