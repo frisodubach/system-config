e1584d856d5f102c7c1cb308a57199b40aef062d
ffcdubach <ffcdubach@gmail.com>
2024-03-04 21:31:15 +0100

    kitty: tab conf update

--- modules/kitty/kitty.nix
+++ modules/kitty/kitty.nix
@@ -17,6 +17,17 @@
         close_on_child_death = true;
         cursor_blink_interval = 0;
         hide_window_decorations = "yes";
+
+        tab_bar_min_tabs = 1;
+        tab_bar_edge = "bottom";
+        # tab_bar_style = "separator";
+        # tab_separator = " | ";
+        tab_bar_style = "powerline";
+        tab_powerline_style = "slanted";
+        # tab_title_template =
+        #   "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
+
+        enabled_layouts = "splits, stack";
       };
 
     };
