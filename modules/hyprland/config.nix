4b930f3b92c27380493b3c864077e99bb468b6b8
ffcdubach <ffcdubach@gmail.com>
2024-08-18 19:02:11 +0800

    hypr: border color update

--- modules/hyprland/config.nix
+++ modules/hyprland/config.nix
@@ -80,7 +80,7 @@
       gaps_in = 5
       gaps_out = 16
       border_size = 2
-      col.active_border = 0xffbdae93
+      col.active_border = 0xff665C54
       col.inactive_border = 0xff282828
       resize_on_border = true
       hover_icon_on_border = true
