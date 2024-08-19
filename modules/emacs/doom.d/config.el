55c867282355dfab8ecd38b0900e61db80745f57
ffcdubach <ffcdubach@gmail.com>
2024-08-19 13:55:00 +0800

    emacs: doom config update, +x sync script

--- modules/emacs/doom.d/config.el
+++ modules/emacs/doom.d/config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Fira Code Nerd Font Mono" :size 14 :weight 'normal)
+(setq doom-font (font-spec :family "Fira Code Nerd Font Mono" :size 16 :weight 'normal)
       doom-variable-pitch-font (font-spec :family "Overpass" :size 16)) ;Overpass okay, but want better font.
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
